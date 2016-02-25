128b224ba0aa688690757f3a2f90a534a73823e4
Guoping Tang <tanggp@yahoo.com>
2016-02-25 09:31:24 -0500

    Added use of phreeqc calculate_values to calculate pH response.

---
+++ phreeqc/microcosm/calculatevalues/ex2som4.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+res = np.loadtxt('ex2som4.txt', skiprows=1)
+rph = np.loadtxt('ex2som4pH.txt', skiprows=1)
+
+totc  = res[:, 1] + res[:, 2] + res[:, 3]*6.0
+
+sc = 365.0
+
+plt.plot(res[:, 0]/sc, res[:, 1], 'b-', res[:, 0]/sc, res[:, 2], 'r-', res[:, 0]/sc, res[:, 3], 'g-', res[:, 0]/sc, totc, 'k-')
+plt.xlabel('Time (y)')
+plt.ylabel('Concentration (mM)')
+lgd = plt.legend(('SOM4', 'CO2', 'Glucose', 'TOTC'),loc=7)
+lgd.draw_frame(False)
+txt = lgd.get_texts()
+plt.setp(txt, fontsize='small') 
+plt.plot(rph[:, 0]/sc, rph[:, 1], 'b-.', rph[:, 0]/sc, rph[:, 2], 'r-.', rph[:, 0]/sc, rph[:, 3], 'g-.', rph[:, 0]/sc, totc, 'k-.')
+
+fig = plt.gcf()
+fig.set_size_inches(6, 4)
+plt.savefig('ex2som4.pdf')
+plt.show()
